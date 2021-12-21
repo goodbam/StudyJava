a5af5e84268e07ff1d630271c1aa19aca7e96906
goodbam <wjdrnqja123@gmail.com>
2021-12-21 11:57:36 +0900

    class 이해하기

--- OOP/_02_Class/Animal.java
+++ OOP/_02_Class/Animal.java
@@ -3,9 +3,22 @@ package OOP._02_Class;
 public class Animal {
     String name; // 객체 변수이고 객체 변수는 공유되지 않는다
 
-    public static void main(String[] args) {
-        Animal cat = new Animal();
+    public void setName(String name){
+        this.name = name; // this는 클래스에 의해 생성된 객체를 지칭한다.
+    }
 
+    public static void main(String[] args) {
+        Animal cat = new Animal(); // "Animal" 자료형인 "cat" 객체 생성
+        cat.setName("body"); // 메서드 호출
+
+        Animal dog = new Animal();
+        dog.setName("happy");
+
+        Animal lion = new Animal();
+        lion.name = "popo";
+
+        System.out.println(lion.name);
         System.out.println(cat.name);
+        System.out.println(dog.name);
     }
 }
